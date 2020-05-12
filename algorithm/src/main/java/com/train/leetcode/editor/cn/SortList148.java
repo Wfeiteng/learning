4807dc09585566af9bf6e781255b8c26350f1538
wangzhengteng <wangzhengteng@guazi.com>
2020-05-12 18:12:23 +0800

    62 63 64

--- algorithm/src/main/java/com/train/leetcode/editor/cn/SortList148.java
+++ algorithm/src/main/java/com/train/leetcode/editor/cn/SortList148.java
@@ -20,6 +20,7 @@ import com.train.leetcode.editor.cn.common.ListNodeUtil;
 import java.util.Arrays;
 
 public class SortList148 {
+
     public static void main(String[] args) {
         ListNode head = ListNodeUtil.buildListNode(Arrays.asList(-1, 5, 3, 4, 0));
         Solution solution = new SortList148().new Solution();
@@ -37,12 +38,74 @@ public class SortList148 {
      * }
      */
     class Solution {
-        // todo
         public ListNode sortList(ListNode head) {
-            if (head != null) {
+            if (head == null) {
                 return head;
             }
-            return null;
+            ListNode first = head, tail = head;
+            while (tail.next != null) {
+                tail = tail.next;
+            }
+            return guibing(first, tail);
+        }
+
+        private ListNode guibing(ListNode head, ListNode tail) {
+            if (head == tail) {
+                return head;
+            }
+            ListNode mid = getMid(head);
+            ListNode l2Start = mid.next;
+            mid.next = null;
+            ListNode l1 = guibing(head, mid);
+            ListNode l2 = guibing(l2Start, tail);
+            return merge(l1, l2);
+        }
+
+        /**
+         * 2个链表合并
+         *
+         * @param l1
+         * @param l2
+         * @return
+         */
+        private ListNode merge(ListNode l1, ListNode l2) {
+            ListNode root = new ListNode(0);
+            ListNode head = root;
+            while (l1 != null && l2 != null) {
+                if (l1.val < l2.val) {
+                    root.next = l1;
+                    l1 = l1.next;
+                } else {
+                    root.next = l2;
+                    l2 = l2.next;
+                }
+                root = root.next;
+            }
+
+            if (l1 != null) {
+                root.next = l1;
+            }
+            if (l2 != null) {
+                root.next = l2;
+            }
+            return head.next;
+        }
+
+        /**
+         * 得到中间值
+         *
+         * @param head
+         * @return
+         */
+        private ListNode getMid(ListNode head) {
+            ListNode root = new ListNode(0);
+            root.next = head;
+            ListNode slow = root, fast = root;
+            while (fast != null && fast.next != null) {
+                fast = fast.next.next;
+                slow = slow.next;
+            }
+            return slow;
         }
 
     }
