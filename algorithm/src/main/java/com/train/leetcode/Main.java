ebe607069e2871001df84ef6e7bdd4926e41c5d8
wangzhengteng <wangzhengteng@guazi.com>
2020-06-03 11:01:51 +0800

    learning:构建树

--- algorithm/src/main/java/com/train/leetcode/Main.java
+++ algorithm/src/main/java/com/train/leetcode/Main.java
@@ -6,6 +6,52 @@ package com.train.leetcode;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("train leetcode");
+        //Scanner in = new Scanner(System.in);
+        //int a = in.nextInt();
+        //System.out.println(a);
+        Main entry = new Main();
+        String str = "5656bascabbacuiuhbab";
+        String subStr = entry.getMaxHuiWen(str);
+        System.out.println(subStr);
+    }
+
+    public String getMaxHuiWen(String str) {
+
+        if (str == null || "".equalsIgnoreCase(str)) {
+            return str;
+        }
+        int length = str.length();
+        int maxLen = 0;
+        int position = 0;
+        char[] strValues = str.toCharArray();
+        for (int i = 0; i < length; i++) {
+            int len1 = getMaxLen(strValues, i, i);
+            int len2 = getMaxLen(strValues, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > maxLen) {
+                maxLen = len;
+                position = i;
+            }
+        }
+        int start = position - (maxLen - 1) / 2;
+        int end = position + maxLen / 2;
+        return str.substring(start, end + 1);
+    }
+
+    private int getMaxLen(char[] str, int left, int right) {
+        int length = str.length;
+        int low = left;
+        int hight = right;
+        while (low >= 0 && hight < length) {
+            if (str[low] == str[hight]) {
+                low--;
+                hight++;
+            } else {
+                break;
+            }
+        }
+        return (hight - low - 1);
     }
 }
+
+
